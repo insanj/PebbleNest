8bf7224bdb4b4c9f6d8debee1047abaa42365cd2
Haifisch <haifisch@Haifischs-MacBook-Air.local>
2013-12-27 23:59:46 -0800

    Eh updates

--- src/PBNestWatch.c
+++ src/PBNestWatch.c
@@ -3,13 +3,17 @@
 static Window *window;
 
 static TextLayer *temperature_layer;
+static TextLayer *humidity_layer;
+
 static char temperature[16];
+static char humidity[16];
 
 static AppSync sync;
 static uint8_t sync_buffer[32];
 
-enum WeatherKey {
-    WEATHER_TEMPERATURE_KEY = 0x0  // TUPLE_CSTRING
+enum HouseKeys {
+    TEMPERATURE_KEY = 0x0,
+    HUMIDITY_KEY = 0x1,
 };
 
 static void sync_error_callback(DictionaryResult dict_error, AppMessageResult app_message_error, void *context) {
@@ -18,35 +22,50 @@ static void sync_error_callback(DictionaryResult dict_error, AppMessageResult ap
 
 static void sync_tuple_changed_callback(const uint32_t key, const Tuple* new_tuple, const Tuple* old_tuple, void* context) {
     switch (key) {
-        case WEATHER_TEMPERATURE_KEY:
+        case TEMPERATURE_KEY:
             // App Sync keeps new_tuple in sync_buffer, so we may use it directly
             text_layer_set_text(temperature_layer, new_tuple->value->cstring);
+            break;
+        case HUMIDITY_KEY:
+            text_layer_set_text(humidity_layer, new_tuple->value->cstring);
             break;
     }
 }
 
 static void window_load(Window *window) {
     Layer *window_layer = window_get_root_layer(window);
-    
-    temperature_layer = text_layer_create(GRect(0, 100, 144, 68));
+    //temperature layer
+    temperature_layer = text_layer_create(GRect(0, 30, 144, 68));
     text_layer_set_text_color(temperature_layer, GColorWhite);
     text_layer_set_background_color(temperature_layer, GColorClear);
     text_layer_set_font(temperature_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
     text_layer_set_text_alignment(temperature_layer, GTextAlignmentCenter);
     text_layer_set_text(temperature_layer, temperature);
-    
+    //humidity layer
+    humidity_layer = text_layer_create(GRect(0, 50, 144, 68));
+    text_layer_set_text_color(humidity_layer, GColorWhite);
+    text_layer_set_background_color(humidity_layer, GColorClear);
+    text_layer_set_font(humidity_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+    text_layer_set_text_alignment(humidity_layer, GTextAlignmentCenter);
+    text_layer_set_text(humidity_layer, humidity);
+    //initial values to set the text as
     Tuplet initial_values[] = {
-        TupletCString(WEATHER_TEMPERATURE_KEY, "1234\u00B0C"),
+        TupletCString(TEMPERATURE_KEY, "Open App"),
+        TupletCString(HUMIDITY_KEY,"..."),
     };
     app_sync_init(&sync, sync_buffer, sizeof(sync_buffer), initial_values, ARRAY_LENGTH(initial_values),
                   sync_tuple_changed_callback, sync_error_callback, NULL);
     
     layer_add_child(window_layer, text_layer_get_layer(temperature_layer));
+    layer_add_child(window_layer, text_layer_get_layer(humidity_layer));
+
 }
 
 static void window_unload(Window *window) {
     app_sync_deinit(&sync);
     text_layer_destroy(temperature_layer);
+    text_layer_destroy(humidity_layer);
+
 }
 
 static void init() {
